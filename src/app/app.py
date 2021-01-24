23e3f81e3b8379baa4f582aa583479b1335a770f
usudoc <yslan380@gmail.com>
2021-01-25 00:15:57 +0900

    Update files

--- src/app/app.py
+++ src/app/app.py
@@ -1,18 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-# 「/」へアクセスがあった場合に、'Hello World'の文字列を返す
 @app.route('/')
-def hello():
-    return 'Hello World'
-
-
-#「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    name = request.args.get('name')
+    spot = ['並榎', '問屋', '県スポ', '山形']
+    return render_template('index.html', name=name, spot=spot)
 
 
 if __name__ == '__main__':
