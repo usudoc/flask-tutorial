03ef450a300af81d3063cebe7b1ad9d59d777e43
usudoc <yslan380@gmail.com>
2021-01-27 18:40:33 +0900

    Update funcs receiving database

--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from models.models import CourtInfo
 
 app = Flask(__name__)
 
@@ -7,15 +8,17 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     name = request.args.get('name')
-    spot = ['並榎', '問屋', '県スポ', '山形']
-    return render_template('index.html', name=name, spot=spot)
+    # spot = ['並榎', '問屋', '県スポ', '山形']
+    all_spot = CourtInfo.query.all()
+    print(all_spot)
+    return render_template('index.html', name=name, all_spot=all_spot)
 
 
 @app.route('/index', methods=['post'])
 def post():
     name = request.form['name']
-    spot = ['並榎', '問屋', '県スポ', '山形']
-    return render_template('index.html', name=name, spot=spot)
+    all_spot = CourtInfo.query.all()
+    return render_template('index.html', name=name, all_spot=all_spot)
 
 
 if __name__ == '__main__':
