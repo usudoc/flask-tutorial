d6064420292636e3ebdc3d7e9cc4b668c2161351
usudoc <yslan380@gmail.com>
2021-01-28 23:00:18 +0900

    Add form adding info

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from models.models import CourtInfo
+from models.database import db_session
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -9,16 +12,45 @@ app = Flask(__name__)
 def index():
     name = request.args.get('name')
     # spot = ['並榎', '問屋', '県スポ', '山形']
-    all_spot = CourtInfo.query.all()
-    print(all_spot)
-    return render_template('index.html', name=name, all_spot=all_spot)
+    all_info = CourtInfo.query.all()
+    print('all_info:', all_info)
+    return render_template('index.html', name=name, all_info=all_info)
 
 
 @app.route('/index', methods=['post'])
 def post():
     name = request.form['name']
-    all_spot = CourtInfo.query.all()
-    return render_template('index.html', name=name, all_spot=all_spot)
+    all_info = CourtInfo.query.all()
+    return render_template('index.html', name=name, all_info=all_info)
+
+
+@app.route('/add', methods=['post'])
+def add():
+    name = request.form['name']
+    type = request.form['type']
+    content = CourtInfo(name, type, datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
+
+
+@app.route('/update', methods=['post'])
+def update():
+    content = CourtInfo.query.filter_by(id=request.form['update']).first()
+    content.name = request.form['name']
+    content.type = request.form['type']
+    db_session.commit()
+    return index()
+
+
+@app.route('/delete', methods=['post'])
+def delete():
+    id_list = request.form.getlist('delete')
+    for id in id_list:
+        content = CourtInfo.query.filter_by(id=id).first()
+        db_session.delete(content)
+    db_session.commit()
+    return index()
 
 
 if __name__ == '__main__':
