342c8ace34ec40dbb56dc6e12fa697b2da9286f3
usudoc <yslan380@gmail.com>
2021-01-30 23:12:00 +0900

    Add users table

--- src/models/models.py
+++ src/models/models.py
@@ -20,12 +20,15 @@ class CourtInfo(Base):
         return '<Name %r>' % (self.name)
 
 
-def _add():
-    c1 = CourtInfo('Taro', 'cray')
-    c2 = CourtInfo('Hanako', 'omni')
-    from database import db_session
-    db_session.add(c1)
-    db_session.add(c2)
-    db_session.commit()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(128), unique=True)
+    hashed_password = Column(String(128))
 
-# _add()
+    def __init__(self, user_name=None, hashed_password=None):
+        self.user_name = user_name
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return '<Name %r>' % (self.user_name)
